89ef24ac115298c7437ca31ca5787e4a3a8d5737
Gustavo Xavier <gustavo.m.x.dev@gmail.com>
2025-09-05 21:35:11 +0000

    new: add forma2d class

---
+++ assessment3/Forma2d.java
@@ -0,0 +1,28 @@
+package assessment3;
+
+public class Forma2d {
+    private String nome;
+
+    public Forma2d(String nome){
+        if(nome != null && nome.equals("")) throw new IllegalArgumentException("O nome não deve ser vazio.");
+        this.nome = nome;
+    }
+
+    public String obterNome(){
+        return this.nome;
+    }
+
+    public void descricao(String nome, double area, double perimetro) {
+        System.out.println("Figura: " + nome);
+        System.out.println("Área: " + area);
+        System.out.println("Perímetro: " + perimetro);
+    }
+
+    public double area(){
+        return 0.0;
+    }
+
+    public double perimetro(){
+        return 0.0;
+    }
+}
